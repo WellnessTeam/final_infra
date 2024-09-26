93ab4fccafe25dc60cdaaf63e2f209b6ae2d040d
노석현 <shtjrgus010@github.com>
2024-09-26 15:35:35 +0900

    Implement EC2: Auto Scaling

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -49,7 +49,7 @@ resource "aws_security_group" "ec2_sg" {
     Environment = var.environment
   }
 }
-
+data "aws_availability_zones" "available" {}
 
 # Amazon Linux 2 AMI를 SSM Parameter Store에서 가져오기
 data "aws_ssm_parameter" "amazon_linux" {
