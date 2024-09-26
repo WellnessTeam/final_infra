5cbe0d53e9343b61c612f0ac63c34eb96e2fe81e
노석현 <shtjrgus010@github.com>
2024-09-26 16:12:15 +0900

    Implement EC2: Auto Scaling

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -82,7 +82,7 @@ resource "aws_autoscaling_group" "app_asg" {
 
   tag {
     key                 = "Name"
-    value               = "{var.environment}-autoscaling"
+    value               = "${var.environment}-autoscaling"
     propagate_at_launch = true
   }
 }
