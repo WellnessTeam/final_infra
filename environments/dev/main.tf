eccdfe1429257abd42012fdd51317f4c64df1570
노석현 <shtjrgus010@github.com>
2024-09-26 14:47:18 +0900

    Implement EC2: Auto Scaling

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -14,7 +14,7 @@ module "ec2" {
   source        = "../../modules/ec2"
   environment   = var.environment
   instance_type = var.instance_type
-  subnet_id     = module.vpc.public_subnets[0]
+  subnet_id     = module.vpc.public_subnets
   vpc_id        = module.vpc.vpc_id
   key_name      = var.key_name
 
