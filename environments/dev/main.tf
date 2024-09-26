ba515247810a5865ed6a36fdc49a6ad4a78fcf8b
노석현 <shtjrgus010@github.com>
2024-09-26 21:33:11 +0900

    fetch: EC2 delete

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -17,7 +17,7 @@ module "ec2" {
   subnet_ids              = module.vpc.public_subnets
   vpc_id                  = module.vpc.vpc_id
   key_name                = var.key_name
-  iam_instance_profile    = module.iam.ec2_instance_role_name
+  iam_instance_profile    = module.iam.ec2_instance_profile_name
   service_linked_role_arn = module.iam.asg_codedeploy_role_arn
 }
 
--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,4 +1,8 @@
 # modules/iam/outputs.tf
+output "ec2_instance_profile_name" {
+  value = aws_iam_instance_profile.ec2_instance_profile.name  # 인스턴스 프로파일 이름을 출력
+}
+
 output "ec2_instance_role_name" {
   value = aws_iam_role.ec2_instance_role.name
 }
